677622831b98969cfa1cf14a71cba19121f31400
tailow <tailows@outlook.com>
2022-04-02 21:59:18 +0300

    Fixed triangle culling

--- Assets/Scripts/Player/TriangleCaster.cs
+++ Assets/Scripts/Player/TriangleCaster.cs
@@ -6,8 +6,12 @@ public class TriangleCaster : MonoBehaviour
 {
     public float cooldown;
 
+    public Material triangleMaterial;
+
     Transform playerCamera;
     LineRenderer lineRenderer;
+    GameObject triangle;
+    Mesh triangleMesh;
 
     int layerMask;
     int pointIndex;
@@ -18,26 +22,57 @@ public class TriangleCaster : MonoBehaviour
         playerCamera = Camera.main.transform;
 
         lineRenderer = GetComponent<LineRenderer>();
+
+        triangle = new GameObject("Triangle");
+
+        triangle.AddComponent<MeshFilter>();
+        triangle.AddComponent<MeshRenderer>();
+
+        triangleMesh = triangle.GetComponent<MeshFilter>().mesh;
+
+        triangleMesh.Clear();
+
+        triangle.GetComponent<MeshRenderer>().material = triangleMaterial;
+        triangle.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        triangle.GetComponent<MeshRenderer>().allowOcclusionWhenDynamic = false;
+
+        triangleMesh.vertices = new Vector3[] {Vector3.zero, Vector3.zero, Vector3.zero};
+        triangleMesh.triangles = new int[] {0, 1, 2};
+
+        triangle.SetActive(false);
     }
 
     void Update(){
         RaycastHit hit;
 
-        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, Mathf.Infinity)){
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, Mathf.Infinity) && pointIndex < 3){
             lineRenderer.SetPosition(pointIndex, hit.point);
+
+            if (pointIndex == 2)
+            {
+                triangleMesh.SetVertices(new Vector3[] {lineRenderer.GetPosition(0), lineRenderer.GetPosition(1), lineRenderer.GetPosition(2)});
+
+                triangleMesh.RecalculateBounds();
+            }
         }
 
-        if (Input.GetMouseButtonDown(0) && Time.time - lastTriangleTime > cooldown){
+        if (Input.GetMouseButtonDown(0) && pointIndex < 3 && Time.time - lastTriangleTime > cooldown){
             lineRenderer.enabled = true;
-
-            lineRenderer.positionCount++;
 
             pointIndex++;
 
-            if (pointIndex == 3){
-                lastTriangleTime = Time.time;
+            if (pointIndex == 2){
+                triangle.SetActive(true);
+            }
 
-                ResetTriangle();
+            // Triangle done
+            if (pointIndex == 3){
+                triangleMesh.RecalculateBounds();
+                lastTriangleTime = Time.time;
+            }
+
+            else {
+                lineRenderer.positionCount++;
             }
         }
 
@@ -52,6 +87,7 @@ public class TriangleCaster : MonoBehaviour
         lineRenderer.SetPosition(0, Vector3.zero);
 
         lineRenderer.enabled = false;
+        triangle.SetActive(false);
 
         pointIndex = 0;
     }
