76ccfc5b182b784aa4f3fd4c86e9833790b61bb2
tailow <tailows@outlook.com>
2022-04-03 17:40:43 +0300

    Added pause menu

--- Assets/Scripts/General Game/GameManager.cs
+++ Assets/Scripts/General Game/GameManager.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
     void Start()
     {
         Cursor.visible = false;
 
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update(){
+        if (Input.GetButtonDown("Exit") && pauseMenu){
+            if (!pauseMenu.activeInHierarchy){
+                pauseMenu.SetActive(true);
+
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+
+                Time.timeScale = 0;
+            }
+
+            else if (pauseMenu.activeInHierarchy){
+                pauseMenu.SetActive(false);
+
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+
+                Time.timeScale = 1;
+            }
+        }
     }
 }
--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
     public void StartGame(){
         SceneManager.LoadScene("scene_main");
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene("scene_menu");
+
+        Time.timeScale = 1;
+    }
+
+    public void Resume(){
+        if (pauseMenu){
+            pauseMenu.SetActive(false);
+
+            Cursor.lockState = CursorLockMode.Locked;
+
+            Time.timeScale = 1;
+        }
     }
 
     public void ExitGame(){
