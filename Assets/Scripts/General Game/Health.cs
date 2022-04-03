f03afe179244f34f7d647015e7427d5e88e0a1c1
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-04-03 10:55:12 +0300

    import healthscript

---
+++ Assets/Scripts/General Game/Health.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Health : MonoBehaviour
+{
+    [HideInInspector]
+    public int health;
+
+    public int baseHealth;
+
+    public Slider healthBar;
+
+    [SerializeField] private AudioClip[] hurtSound;
+    [SerializeField] private AudioSource aS;
+
+    void Start()
+    {
+        health = baseHealth;
+
+        if (healthBar)
+        {
+            healthBar.value = health / baseHealth;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        print(gameObject.name + " " + health);
+
+        if (healthBar)
+        {
+            healthBar.value = health / baseHealth;
+        }
+        if (hurtSound.Length > 0)
+        {
+            aS.clip = (hurtSound[Random.Range(0, hurtSound.Length + 1)]);
+            aS.Play();
+        }
+
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        if (gameObject.tag == "Player")
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+            
+        }
+
+        Destroy(gameObject);
+    }
+}
