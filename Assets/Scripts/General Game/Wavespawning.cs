0173795d29080a3cdf6e3c63dc8ac29e2511ea0f
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-04-04 17:27:37 +0300

    added spawning on upper levels as well

--- Assets/Scripts/General Game/Wavespawning.cs
+++ Assets/Scripts/General Game/Wavespawning.cs
@@ -6,6 +6,7 @@ public class Wavespawning : MonoBehaviour
 {
 
     public GameObject Hammerman;
+    GameObject[] SpawnPoints;
     GameObject player;
     AudioSource aS;
 
@@ -17,6 +18,8 @@ public class Wavespawning : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         aS = GetComponent<AudioSource>();
         Invoke("spawnWave", 2);
+
+        SpawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
     }
 
     void Update()
@@ -30,9 +33,19 @@ public class Wavespawning : MonoBehaviour
     {
         for (int i = 0; i < 4 + wave * 2; i++)
         {
-            Vector3 randomPosition = Random.insideUnitSphere * Random.Range(40, 70);
-            randomPosition.y = 3;
-            Instantiate(Hammerman, randomPosition, Quaternion.identity);
+            Vector3 spawnPosition;
+            if (Random.Range(0, 100) < 90)
+            {
+                spawnPosition = Random.insideUnitSphere * Random.Range(40, 70);
+                spawnPosition.y = 3;
+
+            }
+            else
+            {
+                spawnPosition = SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform.position;
+            }
+
+            Instantiate(Hammerman, spawnPosition, Quaternion.identity);
         }
 
         aS.Play();
